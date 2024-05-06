77b025cde1c43e2ce97fed71d4fe36fe606c9190
JuliusShade <shadejr.business@gmail.com>
2024-05-05 23:09:24 -0400

    Added physics and rules for logic in the game

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+let bird;
+let hasLanded = false;
+let cursors;
+let hasBumped = false;
+
+let isGameStarted = false;
+let messageToPlayer;
 
 function preload() {
   this.load.image("background", "assets/background.png");
@@ -28,9 +35,6 @@ function preload() {
   });
 }
 
-let bird;
-let hasLanded = false;
-
 function create() {
   const background = this.add.image(0, 0, "background").setOrigin(0, 0);
   const roads = this.physics.add.staticGroup();
@@ -40,12 +44,11 @@ function create() {
     setXY: { x: 200, y: 0, stepX: 300 },
   });
 
-  const bottomcolumns = this.physics.add.staticGroup({
+  const bottomColumns = this.physics.add.staticGroup({
     key: "column",
     repeat: 1,
     setXY: { x: 350, y: 400, stepX: 300 },
   });
-
   const road = roads.create(400, 568, "road").setScale(2).refreshBody();
 
   bird = this.physics.add.sprite(0, 50, "bird").setScale(2);
@@ -54,6 +57,68 @@ function create() {
 
   this.physics.add.overlap(bird, road, () => (hasLanded = true), null, this);
   this.physics.add.collider(bird, road);
+
+  this.physics.add.overlap(
+    bird,
+    topColumns,
+    () => (hasBumped = true),
+    null,
+    this
+  );
+  this.physics.add.overlap(
+    bird,
+    bottomColumns,
+    () => (hasBumped = true),
+    null,
+    this
+  );
+  this.physics.add.collider(bird, topColumns);
+  this.physics.add.collider(bird, bottomColumns);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  messageToPlayer = this.add.text(
+    0,
+    0,
+    "Instructions: Press space bar to start",
+    {
+      fontFamily: '"Comic Sans MS", Times, serif',
+      fontSize: "20px",
+      color: "black",
+      backgroundColor: "white",
+    }
+  );
+  Phaser.Display.Align.In.BottomCenter(messageToPlayer, background, 0, 50);
 }
 
-function update() {}
+function update() {
+  if (cursors.space.isDown && !isGameStarted) {
+    isGameStarted = true;
+    messageToPlayer.text =
+      'Instructions: Press the "^" button to stay upright\nAnd don\'t hit the columns or ground';
+  }
+
+  if (!isGameStarted) {
+    bird.setVelocityY(-160);
+  }
+
+  // Move bird upwards
+  if (cursors.up.isDown && !hasLanded && !hasBumped) {
+    bird.setVelocityY(-160);
+  }
+
+  if (isGameStarted && (!hasLanded || !hasBumped)) {
+    bird.body.velocity.x = 50;
+  } else {
+    bird.body.velocity.x = 0;
+  }
+
+  if (hasLanded || hasBumped) {
+    messageToPlayer.text = "Oh no! You crashed!";
+  }
+
+  if (bird.x > 750) {
+    bird.setVelocityY(40);
+    messageToPlayer.text = "Congrats! You won!";
+  }
+}
